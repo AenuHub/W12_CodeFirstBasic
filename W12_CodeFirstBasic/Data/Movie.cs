d53a37c4044a0fdf9a75c1eab17548fb746a808d
AenuHub <jesuskane92@gmail.com>
2024-12-18 16:19:47 +0300

    create Movie.cs

--- W12_CodeFirstBasic/Data/Movie.cs
+++ W12_CodeFirstBasic/Data/Movie.cs
@@ -1,6 +1,22 @@
-﻿namespace W12_CodeFirstBasic.Data;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace W12_CodeFirstBasic.Data;
 
 public class Movie
 {
+    [Required]
+    public int Id { get; set; }
     
+    [MaxLength(100)]
+    public string Title { get; set; } = string.Empty;
+    
+    [MaxLength(100)]
+    public string Genre { get; set; } = string.Empty;
+    
+    // e.g 1980 - 2024
+    [CurrentYearRange(1980)]
+    public int ReleaseYear { get; set; }
 }
+
+// custom validation for release year attribute checking between int min and current year
+public class CurrentYearRangeAttribute(int min) : RangeAttribute(min, DateTime.Now.Year);
