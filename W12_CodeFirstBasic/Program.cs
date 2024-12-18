ccf017368cb2318ce66b4c811380c923ac0268ac
AenuHub <jesuskane92@gmail.com>
2024-12-18 17:55:22 +0300

    update Program.cs

--- W12_CodeFirstBasic/Program.cs
+++ W12_CodeFirstBasic/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace W12_CodeFirstBasic;
+﻿using W12_CodeFirstBasic.Data;
+
+namespace W12_CodeFirstBasic;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
         Console.WriteLine("Testing W12_CodeFirstBasic...");
+
+        using var context = new PatikaFirstDbContext();
+        Console.WriteLine("Games:");
+        foreach (var game in context.Games)
+        {
+            Console.WriteLine($"{game.Id} - {game.Name} - {game.Platform} - {game.Rating}");
+        }
+
+        Console.WriteLine("-----------------------------------------");
+
+        Console.WriteLine("Movies:");
+        foreach (var movie in context.Movies)
+        {
+            Console.WriteLine($"{movie.Id} - {movie.Title} - {movie.Genre} - {movie.ReleaseYear}");
+        }
+        
+        Console.ReadKey();
     }
 }
 
