eeba07c8e29fb96e133156d5169fee7f54efdaa8
AenuHub <jesuskane92@gmail.com>
2024-12-18 16:19:36 +0300

    create Game.cs

--- W12_CodeFirstBasic/Data/Game.cs
+++ W12_CodeFirstBasic/Data/Game.cs
@@ -1,6 +1,18 @@
-﻿namespace W12_CodeFirstBasic.Data;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace W12_CodeFirstBasic.Data;
 
 public class Game
 {
+    [Required]
+    public int Id { get; set; }
     
+    [MaxLength(100)]
+    public string Name { get; set; } = string.Empty;
+    
+    [MaxLength(100)]
+    public string Platform { get; set; } = string.Empty;
+    
+    [Range(0, 10)]
+    public decimal Rating { get; set; }
 }
