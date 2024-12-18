5fbd3fad3c1b91f57424936496974f5086929adb
AenuHub <jesuskane92@gmail.com>
2024-12-18 17:03:08 +0300

    update Movie.cs

--- W12_CodeFirstBasic/Data/Movie.cs
+++ W12_CodeFirstBasic/Data/Movie.cs
@@ -6,15 +6,15 @@ public class Movie
 {
     [Required]
     public int Id { get; set; }
-    
+
     [MaxLength(100)]
     public string Title { get; set; } = string.Empty;
-    
+
     [MaxLength(100)]
     public string Genre { get; set; } = string.Empty;
-    
-    // e.g 1980 - 2024
-    [CurrentYearRange(1980)]
+
+    // e.g 1900 - 2024
+    [CurrentYearRange(1900)]
     public int ReleaseYear { get; set; }
 }
 
