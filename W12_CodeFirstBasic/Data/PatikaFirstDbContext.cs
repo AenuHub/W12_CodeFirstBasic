4e8e8b864fe96a12465395d7f271b004067d7f6a
AenuHub <jesuskane92@gmail.com>
2024-12-18 16:19:54 +0300

    create PatikaFirstDbContext.cs

--- W12_CodeFirstBasic/Data/PatikaFirstDbContext.cs
+++ W12_CodeFirstBasic/Data/PatikaFirstDbContext.cs
@@ -1,6 +1,17 @@
-﻿namespace W12_CodeFirstBasic.Data;
+﻿using Microsoft.EntityFrameworkCore;
 
-public class PatikaFirstDbContext
+namespace W12_CodeFirstBasic.Data;
+
+public class PatikaFirstDbContext : DbContext
 {
+    public DbSet<Movie> Movies { get; set; }
     
+    public DbSet<Game> Games { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        // not the best practice to use hard coded connection string
+        // added test database connection string for learning purposes
+        optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=PatikaCodeFirstDb1;Username=postgres;Password=1234");
+    }
 }
